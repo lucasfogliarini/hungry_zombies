8ce7928aa2e0da3e169bacfc57e25f0260d0242f
Lucas Fogliarini <lucasfogliarini@gmail.com>
2017-02-12 14:00:28 -0200

    create poll tests.

---
+++ api/restaurants/restaurants.tests.js
@@ -0,0 +1,9 @@
+import { Factory } from 'meteor/dburles:factory';
+import { Polls } from './index.js';
+
+describe('Restaurants', function () {
+  describe('#insert', function () {
+    it('should insert without errors', function () {
+    });
+  });
+});
---
+++ api/scores/scores.tests.js
@@ -0,0 +1,9 @@
+import { Factory } from 'meteor/dburles:factory';
+import { Polls } from './index.js';
+
+describe('Scores', function () {
+  describe('#win', function () {
+    it('should win without errors', function () {
+    });
+  });
+});
